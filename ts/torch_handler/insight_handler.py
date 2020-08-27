e4862650ee94567a4eb332f57753902a417fa86b
Hamid Nazeri <Hamid.nazeri2010@gmail.com>
2020-08-27 15:52:09 -0700

    modified to load a custom captum handler

--- ts/torch_handler/insight_handler.py
+++ ts/torch_handler/insight_handler.py
@@ -10,6 +10,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import torch.nn as nn
+import importlib
 from torch.nn.utils.rnn import pad_sequence
 from typing import Any, Iterable, List, Tuple, Union
 from captum.attr import visualization as viz
@@ -19,43 +20,13 @@ from ts.torch_handler import captum_utils
 # from ts.utils import captum_utils
 from enum import Enum
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoModelForQuestionAnswering,AutoModelForTokenClassification,AutoConfig
+import importlib.machinery
 
-from ts.torch_handler.word_importance import WordImportance
-
-class InsightMethod(Enum):
-    word_importance = "WordImportance"
-
-
-    def __str__(self):
-        return self.value
 
 class Insight():
 
-    def __init__(self, method:InsightMethod,service, request):
-        self.insight_method = method
-        print("method: ",self.insight_method)
+    def __init__(self,service):
         self.service = service
-        self.request = request
-        # self.tokenizer = service.tokenizer
-        # self.setup_config = service.setup_config
-        # self.mapping = service.mapping
-        # self.request = request
-        if self.insight_method is InsightMethod.word_importance:
-            print("comparing methods: ",self.insight_method, InsightMethod.word_importance)
-
-            self.attribution = WordImportance(self.service.model)
-
-
-    def insight(self,**args):
-        try:
-            if self.insight_method is InsightMethod.word_importance:
-                dom = self.attribution.get_insight(tokenizer = self.service.tokenizer, setup_config = self.service.setup_config,request = self.request, mapping = self.service.mapping)
-                return dom
-        except Exception as e:
-            raise e
-
-#
-# if __name__ == "__main__":
-#     if request["interperation"]:
-#         insight_handler = Insight(InsightMethod("WordImportance"),model)
-#         insight_handler.insight(tokenizer = tokenizer, setup_config = setup_config,request = request, mapping = mapping)
+        directory, module_name = os.path.split(self.service.captum_handler_path)
+        module_name = os.path.splitext(module_name)[0]
+        self.captum_handler = __import__(module_name)
